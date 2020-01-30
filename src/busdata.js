e698ebac7022f8392340e71d722b13f988da22ac
Karthik kumar <mkarthikkumar19@gmail.com>
2020-01-30 23:08:59 +0530

    dynamic search input is gathered

--- src/busdata.js
+++ src/busdata.js
@@ -5,22 +5,43 @@ import firebase from './firebase';
 class busdata extends React.Component{
 
 state={
-    data:[]
+    data:[],
+    Startingpoint:'',
+    Destination:'',
+   
 }
 // componentDidMount(){
 //     this.searchPage();
 // }
+
+updateInput(event){
+    let name = event.target.value;
+    let Startingpoint = this.state.Startingpoint;
+    Startingpoint = name;
+    this.setState({Startingpoint:Startingpoint});
+    }
+
+    updatesecInput(event){
+        let name = event.target.value;
+        let Destination = this.state.Destination;
+        Destination = name;
+        this.setState({Destination});
+        }
+
  setst = (data) => {
     this.setState({data:data});
+    console.log(this.state.Destination);
     console.log(this.state.data);
     console.log(this.state.data.length)
 }
- searchPage = () => {
+ searchPage = (event,no,start,des) => {
+     console.log(start,des);
+     event.preventDefault();
     var slice="";
         const ref = firebase.database().ref('buses');
         ref
   .orderByChild('no')
-  .equalTo("48")
+  .equalTo(no)
   .on('value', function(snapshot) { 
       var movie = snapshot.val();
       console.log(movie);
@@ -29,16 +50,19 @@ state={
         //   if(movie[name].stops)
         let stop = movie[name].names;
         console.log(stop);
-        var ind1,ind2;
+        let ind1,ind2;
         for(let name in stop){
-            if(stop[name].stopname === "Anagar"){
+            if(stop[name].stopname === start){
                  ind1 =name;
-                console.log(stop[name].data);
-            }else if(stop[name].stopname === "Tpk"){
-                 ind2 = name;
+                console.log(name);
+            }else if(stop[name].stopname === des){
+                console.log(name)
+                 ind2 = name ;
+               ind2=  Number(ind2) + 1;
             }
         }
-         slice = stop.slice(ind1,ind2+1);
+        console.log(ind2);
+         slice = stop.slice(ind1,ind2);
           console.log(slice);
           slice.map((page,index) => (
             console.log(page.stopname)
@@ -74,8 +98,12 @@ if(this.state.data.length > '1'){
 
         return(
             <div>
-                <h1>hello</h1>
-                <button onClick={(event) => this.searchPage(event)}>Click here</button>
+                <h1>Search your Bus</h1>
+                <form  >
+                    <input type="text" placeholder="enter the Starting Point name" onChange={(event) => this.updateInput(event)}></input>
+                    <input type="text" placeholder="enter the Destination name" onChange={(event) => this.updatesecInput(event)}></input>
+                <button onClick={(event) => this.searchPage(event,"48",this.state.Startingpoint,this.state.Destination)} >Search</button>
+                    </form>    
                 {name}
             </div>
         )
