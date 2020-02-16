844149c172f73be1c4e1f9b84a4f03c8543a9d04
Karthik kumar <mkarthikkumar19@gmail.com>
2020-02-16 15:58:42 +0530

    utility and index.js updated

--- src/index.js
+++ src/index.js
@@ -7,11 +7,24 @@ import './index.css';
 import App from './App';
 import thunk from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
+import dataReducer from './store/reducers/data';
+
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+
+const rootReducer = combineReducers({
+    data : dataReducer,
+});
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 const app = (
+    <Provider store={store}>
     <BrowserRouter>
     <App/>
     </BrowserRouter>
+    </Provider>
 )
 
 ReactDOM.render(app, document.getElementById('root'));
