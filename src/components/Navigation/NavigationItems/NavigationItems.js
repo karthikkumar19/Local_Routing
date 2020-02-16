4830cf252522a0a5305660d091ad1e791b255449
Karthik kumar <mkarthikkumar19@gmail.com>
2020-02-16 10:45:12 +0530

    react router dom is installed

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,15 +6,8 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
-        <h1 className={classes.DesktopOnly}>Instagram Pages</h1>
-        <div className={classes.dropdown}>
-  <button className={classes.dropbtn}>Filter</button>
-  <div className={classes.dropdowncontent}>
-  <button onClick={props.ascpage} >Low To High</button>
-  <button onClick={props.dscpage} >High To Low</button>
-  </div>
-</div>
-       { props.isAuthenticated ? <NavigationItem link="/add">Add page</NavigationItem> : null }
+
+       { props.isAuthenticated ? <NavigationItem link="/add">Add Bus Data</NavigationItem> : null }
         {
             !props.isAuthenticated ? <NavigationItem link="/auth">Admin</NavigationItem> 
             : <NavigationItem link="/logout">Logout</NavigationItem>
