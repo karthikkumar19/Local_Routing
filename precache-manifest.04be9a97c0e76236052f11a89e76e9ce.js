7292de53fee02ec7b8385bbd6ee1217ea5b387b0
Karthik kumar <mkarthikkumar19@gmail.com>
2020-04-18 13:22:08 +0530

    Updates

---
+++ precache-manifest.04be9a97c0e76236052f11a89e76e9ce.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca142c0fed6412ba5b3c85b3259b9ac",
+    "url": "/Local_Routing/index.html"
+  },
+  {
+    "revision": "eab9a83cd269464f76e1",
+    "url": "/Local_Routing/static/css/3.e7776c51.chunk.css"
+  },
+  {
+    "revision": "9b172838144d806a1a90",
+    "url": "/Local_Routing/static/css/4.6e5cc90d.chunk.css"
+  },
+  {
+    "revision": "806df547605d3971ea38",
+    "url": "/Local_Routing/static/css/5.39568324.chunk.css"
+  },
+  {
+    "revision": "e3dec69e0df5dc92d8f6",
+    "url": "/Local_Routing/static/css/6.0d16cecb.chunk.css"
+  },
+  {
+    "revision": "23811b793554d32446e0",
+    "url": "/Local_Routing/static/css/7.c5eef1c3.chunk.css"
+  },
+  {
+    "revision": "6da1202f80524b608525",
+    "url": "/Local_Routing/static/css/main.daab2bd3.chunk.css"
+  },
+  {
+    "revision": "6feab5b79af0fe08295d",
+    "url": "/Local_Routing/static/js/0.146cf1e2.chunk.js"
+  },
+  {
+    "revision": "ff20679d6509f5c00d66e84bd9025fd6",
+    "url": "/Local_Routing/static/js/0.146cf1e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eab9a83cd269464f76e1",
+    "url": "/Local_Routing/static/js/3.5fe8341f.chunk.js"
+  },
+  {
+    "revision": "5586a95853ee43deda1fb4da37dc3fc8",
+    "url": "/Local_Routing/static/js/3.5fe8341f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b172838144d806a1a90",
+    "url": "/Local_Routing/static/js/4.3d16e985.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/Local_Routing/static/js/4.3d16e985.chunk.js.LICENSE"
+  },
+  {
+    "revision": "806df547605d3971ea38",
+    "url": "/Local_Routing/static/js/5.d6f0475e.chunk.js"
+  },
+  {
+    "revision": "e3dec69e0df5dc92d8f6",
+    "url": "/Local_Routing/static/js/6.9ebf1400.chunk.js"
+  },
+  {
+    "revision": "23811b793554d32446e0",
+    "url": "/Local_Routing/static/js/7.ced550c8.chunk.js"
+  },
+  {
+    "revision": "6da1202f80524b608525",
+    "url": "/Local_Routing/static/js/main.98198e7e.chunk.js"
+  },
+  {
+    "revision": "f62aa46f52e3a7f773da",
+    "url": "/Local_Routing/static/js/runtime-main.7d59803b.js"
+  },
+  {
+    "revision": "7a1f000a26c1d2081855a4cf226c5c5c",
+    "url": "/Local_Routing/static/media/busbg1.7a1f000a.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Local_Routing/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Local_Routing/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Local_Routing/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Local_Routing/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Local_Routing/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
